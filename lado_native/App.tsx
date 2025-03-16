6e979f9968109bdf62c930ad1d4cadf9afdd396f
Dilshad Ansari <94075679+dilshadansari12@users.noreply.github.com>
2025-03-16 16:04:55 +0530

    Fixed tracking of lado_native

---
+++ lado_native/App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const safePadding = '5%';
+
+  return (
+    <View >
+      <Text style={{ textAlign: "center", marginTop: 500 }}>Hello React Native  ok</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
